6cf4f03593c38a4241e1554576ea619a59261f2e
Sebastián Jaque <jaquesebastian0@gmail.com>
2025-07-25 04:23:10 +0000

    No me gusta el color, puedes hacerlo de otro para hacerlo mas bonito per

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,21 @@ import { CodeGate } from '@/components/CodeGate';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center p-8 bg-grid-primary/5">
-      <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-[radial-gradient(#84A56833_1px,transparent_1px)] [background-size:32px_32px]"></div>
+    <main className="relative flex min-h-screen w-full flex-col items-center justify-center p-8 overflow-hidden">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-background">
+        <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
       <CodeGate />
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Alegreya', 'serif'],
-        headline: ['Alegreya', 'serif'],
+        body: ['Montserrat', 'sans-serif'],
+        headline: ['Montserrat', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
